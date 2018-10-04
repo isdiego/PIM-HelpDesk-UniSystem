dc30c1dc48987edd38958c98105c0d9de175fb9d
Diego <diego.fmm96@gmail.com>
2018-10-04 14:19:14 -0300

    Criação dos Controllers: Chamado, CrudUsuario, Home!
    Crud dos usuarios
    
    Conexão com o BD!

---
+++ UniSystemHelpDesk/Controllers/CrudUsuariosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UniSystemHelpDesk.Models;
+
+namespace UniSystemHelpDesk.Controllers
+{
+    public class CrudUsuariosController : Controller
+    {
+        private UniSystemBD db = new UniSystemBD();
+
+        // GET: CrudUsuarios
+        public ActionResult Index()
+        {
+            var uS_USUARIOS = db.US_USUARIOS.Include(u => u.US_SETORES).Include(u => u.US_TIPO_USUARIO);
+            return View(uS_USUARIOS.ToList());
+        }
+
+        // GET: CrudUsuarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            US_USUARIOS uS_USUARIOS = db.US_USUARIOS.Find(id);
+            if (uS_USUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uS_USUARIOS);
+        }
+
+        // GET: CrudUsuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_SETOR = new SelectList(db.US_SETORES, "ID_SETOR", "SETOR");
+            ViewBag.ID_TIPO_USUARIO = new SelectList(db.US_TIPO_USUARIO, "ID_TIPO_USUARIO", "TIPO_USUARIO");
+            return View();
+        }
+
+        // POST: CrudUsuarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_USUARIOS,NOME_USUARIO,EMAIL_USUARIO,SENHA_USUARIO,ID_TIPO_USUARIO,ID_SETOR")] US_USUARIOS uS_USUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.US_USUARIOS.Add(uS_USUARIOS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_SETOR = new SelectList(db.US_SETORES, "ID_SETOR", "SETOR", uS_USUARIOS.ID_SETOR);
+            ViewBag.ID_TIPO_USUARIO = new SelectList(db.US_TIPO_USUARIO, "ID_TIPO_USUARIO", "TIPO_USUARIO", uS_USUARIOS.ID_TIPO_USUARIO);
+            return View(uS_USUARIOS);
+        }
+
+        // GET: CrudUsuarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            US_USUARIOS uS_USUARIOS = db.US_USUARIOS.Find(id);
+            if (uS_USUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_SETOR = new SelectList(db.US_SETORES, "ID_SETOR", "SETOR", uS_USUARIOS.ID_SETOR);
+            ViewBag.ID_TIPO_USUARIO = new SelectList(db.US_TIPO_USUARIO, "ID_TIPO_USUARIO", "TIPO_USUARIO", uS_USUARIOS.ID_TIPO_USUARIO);
+            return View(uS_USUARIOS);
+        }
+
+        // POST: CrudUsuarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_USUARIOS,NOME_USUARIO,EMAIL_USUARIO,SENHA_USUARIO,ID_TIPO_USUARIO,ID_SETOR")] US_USUARIOS uS_USUARIOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uS_USUARIOS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_SETOR = new SelectList(db.US_SETORES, "ID_SETOR", "SETOR", uS_USUARIOS.ID_SETOR);
+            ViewBag.ID_TIPO_USUARIO = new SelectList(db.US_TIPO_USUARIO, "ID_TIPO_USUARIO", "TIPO_USUARIO", uS_USUARIOS.ID_TIPO_USUARIO);
+            return View(uS_USUARIOS);
+        }
+
+        // GET: CrudUsuarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            US_USUARIOS uS_USUARIOS = db.US_USUARIOS.Find(id);
+            if (uS_USUARIOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uS_USUARIOS);
+        }
+
+        // POST: CrudUsuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            US_USUARIOS uS_USUARIOS = db.US_USUARIOS.Find(id);
+            db.US_USUARIOS.Remove(uS_USUARIOS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UniSystemHelpDesk/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniSystemHelpDesk.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace UniSystemHelpDesk.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(US_USUARIOS lc)
+        {
+            string mainconexao = ConfigurationManager.ConnectionStrings["ConexaoBD"].ConnectionString;
+            SqlConnection sqlconexao = new SqlConnection(mainconexao);
+            string sqlquery = "select EMAIL_USUARIO, SENHA_USUARIO from [dbo].[US_USUARIOS] where EMAIL_USUARIO=@EMAIL_USUARIO and SENHA_USUARIO=@SENHA_USUARIO";
+            sqlconexao.Open();
+            SqlCommand sqlcomando = new SqlCommand(sqlquery, sqlconexao);
+            sqlcomando.Parameters.AddWithValue("@EMAIL_USUARIO", lc.EMAIL_USUARIO);
+            sqlcomando.Parameters.AddWithValue("@SENHA_USUARIO", lc.SENHA_USUARIO);
+            SqlDataReader sdr = sqlcomando.ExecuteReader();
+            if (sdr.Read())
+            {
+                Session["username"] = lc.EMAIL_USUARIO.ToString();
+                return RedirectToAction("BemVindo");
+            }
+            else
+            {
+                ViewData["Mensagem"] = "Erro: Usuário ou Senha invalida !";
+            }
+            sqlconexao.Close();
+            return View();
+        }
+
+        public ActionResult BemVindo()
+        {
+            return View();
+        }
+    }
+}
